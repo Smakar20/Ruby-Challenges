06307ad024ddf619c5fc2e6219ff65668e9d08fb
Sohini Makar <sohini.makar@gmail.com>
2016-07-12 13:07:49 -0700

    anagram
    anagram update

--- anagram.rb
+++ anagram.rb
@@ -1,15 +1,18 @@
-#Anagram check.
+#Anagram array check.
 def anagram(arr)
 	result = []
+	deleted_store = []
 	arr.each {|word|
-		anagram_arr = [word]
-		letter_arr = word.split("").sort
-		arr.length.times  {|i| anagram_arr << arr[i] if arr[i].split("").sort == letter_arr && anagram_arr.include?(arr[i])== false}
-		arr.each {|ana| arr.delete(ana) if anagram_arr.include?(ana)}
+		next if deleted_store.include?(word)
+		anagram_arr = []
+		letter_arr = word.downcase.split("").sort
+		arr.length.times  {|i| anagram_arr << arr[i] if arr[i].downcase.split("").sort == letter_arr #&& !anagram_arr.include?(arr[i]) #requried to exclude duplicates
+		}
+		anagram_arr.each {|ana| deleted_store << ana}
 		result << anagram_arr
 	}
 	result
 end
 
-arr =["abc","bca","acb","acd","dca","tvf","xyz","zyx","xzy"]
-p anagram(arr)
+arr =["Abc","acb","bCa","acb","acd","dca","TVF","xyz","zyx","xzy","RsT"]
+anagram(arr)
